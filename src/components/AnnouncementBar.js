f965cb9be3a1f6153f6712772edafdb9a92c679f
Vignesh <vicky86088@gmail.com>
2023-11-25 04:46:31 +0530

    responsive design

--- src/components/AnnouncementBar.js
+++ src/components/AnnouncementBar.js
@@ -1,9 +1,5 @@
-// import localFont from "next/font/local";
 import Link from "next/link";
 
-// const myFont = localFont({
-//   src: "./fonts/MADE_TOMMY_Regular.otf",
-// });
 
 export default function AnnouncementBar() {
   return <div className="announcement">
